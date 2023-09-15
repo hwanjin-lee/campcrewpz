e3c72cf959c8f5ebd870c79e0f1263641ec4cf5d
Hwanjin <hwanjin.lee174@gmail.com>
2023-09-14 21:58:22 -0700

    kofi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ReactDOM } from "react";
+// import { ReactDOM } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Test from "./pages/Test";
